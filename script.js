d69cced2557b9da0dad1a20362e4bb8dd4d45878
Dahir Mohamed <dmoabdi98@gmail.com>
2021-09-29 18:21:15 +0100

    added user choice for content of PW with confirm

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //                       STEP 1 :
 // prompt bar asking the user to input a character length
 // Also convert that number string in to a number type data
+
 const userPasswordLength = function () {
   const characterLength = prompt(
     `Enter the character length the of the password `,
@@ -9,33 +10,60 @@ const userPasswordLength = function () {
   const inputNumValue = Number(characterLength); // the input string is converted to a number
 
   //                       STEP 2 :
-  // function that validates the user input matches the password length criterial
-  // function passwordLengthCriteria() {
-  //   if (userPasswordLength() < 8 && userPasswordLength() > 125) {
-  //     alert(`Password length is not valid. Pick a length between 8 and 125`);
-  //   }
-  // }
+  // if statement that validates the user input matches the password length criterial
 
   if (inputNumValue < 8 || inputNumValue > 125) {
     return alert(
-      `Password length is not valid. Pick a length between 8 and 125`
+      `Password length is not valid. Pick a length between 8 and 125 and press cancel`
     );
   } else return inputNumValue;
 };
 
 console.log(userPasswordLength());
-
 //                       STEP 3 :
 //  function for confirm bar for whether the user wants lowercase characters in password
+
+function lowerCaseOption() {
+  const option = confirm(
+    "Press OK if you want your password to have lowercase characters"
+  );
+  if (option === true) return `chose lowercase characters`;
+  else return `dont want any lower case charaters`;
+}
+console.log(lowerCaseOption());
 
 //                       STEP 4 :
 //  function for confirm bar for whether the user wants uppercase characters in password
 
+function upperCaseOption() {
+  const option = confirm(
+    "Press OK if you want your password to have uppercase characters"
+  );
+  if (option === true) return `chose uppercase characters`;
+  else return `dont want any uppercase charaters`;
+}
+console.log(upperCaseOption());
+
 //                       STEP 5 :
 //  function for confirm bar for whether the user wants numbers characters in password
 
+function numericOption() {
+  const option = confirm("Press OK if you want your password to have Numbers");
+  if (option === true) return `chose numbers`;
+  else return `dont want any number charaters`;
+}
+console.log(numericOption());
+
 //                       STEP 6 :
 //  function for confirm bar for whether the user wants special characters in password
+function specialCharacOption() {
+  const option = confirm(
+    "Press OK if you want your password to have special characters"
+  );
+  if (option === true) return `chose special characters`;
+  else return `dont want any special charaters`;
+}
+console.log(specialCharacOption());
 
 //                       STEP 7 :
 //  declare a list of lowercase , uppercase , numbers and special characters in a string
