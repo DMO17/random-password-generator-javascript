8e2e65dd7d4102b2217154ede777d2efdd962c0e
Dahir Mohamed <dmoabdi98@gmail.com>
2021-09-29 17:23:16 +0100

    project plan unitil gathering the user input data

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// prompt bar showing
+//                       STEP 1 :
+// prompt bar asking the user to input a character length
+// Also convert that number string in to a number type data
+
+//                       STEP 2 :
+// function that validates the user input matches the password length criterial
+
+//                       STEP 3 :
+//  function for confirm bar for whether the user wants lowercase characters in password
+
+//                       STEP 4 :
+//  function for confirm bar for whether the user wants uppercase characters in password
+
+//                       STEP 5 :
+//  function for confirm bar for whether the user wants numbers characters in password
+
+//                       STEP 6 :
+//  function for confirm bar for whether the user wants special characters in password
+
+//                       STEP 7 :
+//  declare a list of lowercase , uppercase , numbers and special characters in a string
+// convert the string into an array
