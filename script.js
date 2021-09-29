36f0176158b3a819d443ac2b043a67f363f09838
Dahir Mohamed <dmoabdi98@gmail.com>
2021-09-29 18:03:26 +0100

    added the validation of PW lenght in the same func

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 //                       STEP 1 :
 // prompt bar asking the user to input a character length
 // Also convert that number string in to a number type data
-function userPasswordLength() {
+const userPasswordLength = function () {
   const characterLength = prompt(
     `Enter the character length the of the password `,
     `Enter Here`
   );
-  return Number(characterLength); // the input string is converted to a number
-}
+  const inputNumValue = Number(characterLength); // the input string is converted to a number
 
-//                       STEP 2 :
-// function that validates the user input matches the password length criterial
+  //                       STEP 2 :
+  // function that validates the user input matches the password length criterial
+  // function passwordLengthCriteria() {
+  //   if (userPasswordLength() < 8 && userPasswordLength() > 125) {
+  //     alert(`Password length is not valid. Pick a length between 8 and 125`);
+  //   }
+  // }
+
+  if (inputNumValue < 8 || inputNumValue > 125) {
+    return alert(
+      `Password length is not valid. Pick a length between 8 and 125`
+    );
+  } else return inputNumValue;
+};
+
+console.log(userPasswordLength());
 
 //                       STEP 3 :
 //  function for confirm bar for whether the user wants lowercase characters in password
